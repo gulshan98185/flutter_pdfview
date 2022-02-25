f205982f2388cc33624f24cc9d451195109552c3
abhijeet <sabhijeet410@gmail.com>
2022-02-25 12:23:31 +0530

    bug fix

--- lib/get_pdf_thumbnail.dart
+++ lib/get_pdf_thumbnail.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 
 Future<bool?> getPdfThumbNail(String pdfFilePath, String destThumbPath) async {
-  MethodChannel _channel =   MethodChannel(Platform.isAndroid?"Pdf_To_Image":'native_pdf_tools');
+  MethodChannel _channel = MethodChannel(Platform.isAndroid ? "Pdf_To_Image" : 'native_pdf_tools');
   return await _channel.invokeMethod('getPdfThumbNail', {
     'pdfFilePath': pdfFilePath,
     'destThumbPath': destThumbPath,
